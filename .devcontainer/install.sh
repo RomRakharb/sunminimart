2ddac84a559e7f5a91b64166125c852fb3f608f2
Rom Rakharb <121065878+RomRakharb@users.noreply.github.com>
2025-04-08 14:51:32 +0700

    Initial commit

---
+++ .devcontainer/install.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/bash
+
+echo
+echo "---------------------"
+echo "starts install script"
+echo "---------------------"
+echo
+
+sudo apt update && sudo apt -y upgrade
+npm install npm@latest -g
+
+git clone https://github.com/helix-editor/helix.git
+cargo install --path ./helix/helix-term --locked
+hx --grammar fetch
+hx --grammar build
+cp -r ./helix/runtime/queries ~/.config/helix/runtime/
+cp -r ./helix/runtime/themes ~/.config/helix/runtime/
+cp ./helix/runtime/tutor ~/.config/helix/runtime/
+echo -e "[editor]\ntrue-color = true" > ~/.config/helix/config.toml
+rm -f helix -R
+
+cargo install bacon --locked
+cargo install cargo-leptos --locked
+cargo install leptosfmt --locked
+
+rustup component add rust-analyzer
+npm i -g vscode-langservers-extracted
+rustup target add wasm32-unknown-unknown
+
+mkdir -p ~/.helix
+echo '[[language]]
+name = "rust"
+
+[language-server.rust-analyzer.config]
+cargo = { allFeatures = true }
+check.command = "clippy"' > ~/.helix/languages.toml
+
+echo
+echo "---------------------"
+echo "install script ends"
+echo "---------------------"
+echo
