1f89147dd560f6bd4c61827c9577d857813b99b9
Rom Rakharb <romr.dev@proton.me>
2025-06-12 21:11:20 +0700

    add database

---
+++ migrations/20250611065506_base_schema.sql
@@ -0,0 +1,27 @@
+-- Add migration script here
+USE sunminimart;
+
+CREATE TABLE IF NOT EXISTS items (
+    barcode VARCHAR(64) PRIMARY KEY,
+    name VARCHAR(64) NOT NULL,
+    cost DECIMAL(6, 2) UNSIGNED NOT NULL,
+    price DECIMAL(6, 2) UNSIGNED NOT NULL,
+    amount SMALLINT NOT NULL,
+    image BLOB
+);
+
+CREATE TABLE IF NOT EXISTS bulk_items (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    barcode VARCHAR(64),
+    ref_barcode VARCHAR(64) NOT NULL,
+    amount SMALLINT NOT NULL,
+    price DECIMAL(6, 2) UNSIGNED NOT NULL,
+    FOREIGN KEY (ref_barcode) REFERENCES items(barcode) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS expire_dates (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    barcode VARCHAR(64) NOT NULL,
+    expire_date DATE NOT NULL,
+    FOREIGN KEY (barcode) REFERENCES items(barcode) ON DELETE CASCADE ON UPDATE CASCADE
+);
